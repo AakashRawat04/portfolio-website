09a0662a2d774bfd15ffa93ab1c1de5ed09c62f5
Ash <01tripster@gmail.com>
2023-02-18 19:27:14 +0530

    Package json Changed

---
+++ src/overlay.jsx
@@ -0,0 +1,17 @@
+import { useLayoutEffect } from "react";
+
+
+const AllPages = () => {
+
+     useLayoutEffect(()=>{
+          
+     }, [])
+     
+     return(
+          <div className="overlay">
+               <img src="sldfjk" alt="hexaicon"></img>
+          </div>
+     )
+}
+
+export default AllPages;
