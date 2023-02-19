2dab97db49b072f8ed5de8ca7e53128eb8fe2020
Ash <01tripster@gmail.com>
2023-02-20 03:40:31 +0530

    redirect to social media accounts when click on icons

--- src/overlay.jsx
+++ src/overlay.jsx
@@ -10,26 +10,40 @@ const Overlay = () => {
      useLayoutEffect(()=>{
           gsap.from(".overlay", {
                // y: 50,
-               x: -100,
+               // x: -50,
                opacity: 0,
                scrollTrigger: {
                     trigger: ".overlay",
-                    start: "top center",
+                    start: "top 10%",
                     end: "top top",
                     scrub: 1,
                }
           })
      }, [])
+
+     const githublink = "https://github.com/tripsterxx"
+     const linkedinlink = "https://www.linkedin.com/in/aakash-rawat-a1a528236/"
+     const twitterlink = "https://twitter.com/rawatAsh2004"
+     const instagramlink = "https://www.instagram.com/rawatash04/"
      
      return(
           <div className="overlay">
                <img src={hexalogo} alt="hexalogo" id="hexalogo"></img>
 
                <div className="lefticons">
-                    <img src="./pp_items/lefticons/githublogo.svg" alt="lefticons"></img>
-                    <img src="./pp_items/lefticons/linkedinlogo.svg" alt="lefticons"></img>
-                    <img src="./pp_items/lefticons/twitterlogo.svg" alt="lefticons"></img>
-                    <img src="./pp_items/lefticons/instagramlogo.svg" alt="lefticons"></img>
+                    <a href={githublink} target="_blank" rel="noreferrer">
+                         <img src="./pp_items/lefticons/githublogo.svg" alt="lefticons"></img>
+                    </a>
+                    <a href={linkedinlink}>
+                         <img src="./pp_items/lefticons/linkedinlogo.svg" alt="lefticons"></img>
+                    </a>
+                    <a href={twitterlink}>
+                         <img src="./pp_items/lefticons/twitterlogo.svg" alt="lefticons"></img>
+                    </a>
+                    <a href={instagramlink}>
+                         <img src="./pp_items/lefticons/instagramlogo.svg" alt="lefticons"></img>
+                    </a>
+                    
                </div>
                <div id="lefticonbar"></div>
           </div>
